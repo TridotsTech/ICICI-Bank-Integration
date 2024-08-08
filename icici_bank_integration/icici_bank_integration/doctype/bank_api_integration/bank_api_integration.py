ff09fd4179ffe05d3e7722f2a9987d2c47525c45
Tridots Tech <info@tridotstech.com>
2024-08-08 06:54:39 +0000

    fatal:Update the transaction api parameter CUSTOMERINDUCED to Y

--- icici_bank_integration/icici_bank_integration/doctype/bank_api_integration/bank_api_integration.py
+++ icici_bank_integration/icici_bank_integration/doctype/bank_api_integration/bank_api_integration.py
@@ -48,7 +48,7 @@ def initiate_transaction_with_otp(docname, otp):
 				'party': doc.party,
 				'is_default': 1
 				},'ifsc_code'),
-		"AMOUNT": round(doc.amount,2),
+		"AMOUNT": float("{:.2f}".format(doc.amount)),
 		"CURRENCY": currency,
 		"TXNTYPE": doc.transaction_type,
 		"PAYEENAME": doc.party,
